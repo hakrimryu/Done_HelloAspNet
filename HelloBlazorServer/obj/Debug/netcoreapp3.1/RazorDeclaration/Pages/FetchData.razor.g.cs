96bb58dd54c5628878b295f5f5f76b7eac996b89
hakrimryu <hakrimryu@gmail.com>
2021-07-25 15:04:40 +0900

    ゴマゴマ

--- HelloBlazorServer/obj/Debug/netcoreapp3.1/RazorDeclaration/Pages/FetchData.razor.g.cs
+++ HelloBlazorServer/obj/Debug/netcoreapp3.1/RazorDeclaration/Pages/FetchData.razor.g.cs
@@ -13,70 +13,70 @@ namespace HelloBlazorServer.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\Ryu\Documents\ASPTest\HelloAspNet\HelloBlazorServer\_Imports.razor"
+#line 1 "C:\Users\Ryu\Documents\HelloAspNet\HelloBlazorServer\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Ryu\Documents\ASPTest\HelloAspNet\HelloBlazorServer\_Imports.razor"
+#line 2 "C:\Users\Ryu\Documents\HelloAspNet\HelloBlazorServer\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Ryu\Documents\ASPTest\HelloAspNet\HelloBlazorServer\_Imports.razor"
+#line 3 "C:\Users\Ryu\Documents\HelloAspNet\HelloBlazorServer\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Ryu\Documents\ASPTest\HelloAspNet\HelloBlazorServer\_Imports.razor"
+#line 4 "C:\Users\Ryu\Documents\HelloAspNet\HelloBlazorServer\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Ryu\Documents\ASPTest\HelloAspNet\HelloBlazorServer\_Imports.razor"
+#line 5 "C:\Users\Ryu\Documents\HelloAspNet\HelloBlazorServer\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Ryu\Documents\ASPTest\HelloAspNet\HelloBlazorServer\_Imports.razor"
+#line 6 "C:\Users\Ryu\Documents\HelloAspNet\HelloBlazorServer\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Ryu\Documents\ASPTest\HelloAspNet\HelloBlazorServer\_Imports.razor"
+#line 7 "C:\Users\Ryu\Documents\HelloAspNet\HelloBlazorServer\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Ryu\Documents\ASPTest\HelloAspNet\HelloBlazorServer\_Imports.razor"
+#line 8 "C:\Users\Ryu\Documents\HelloAspNet\HelloBlazorServer\_Imports.razor"
 using HelloBlazorServer;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Ryu\Documents\ASPTest\HelloAspNet\HelloBlazorServer\_Imports.razor"
+#line 9 "C:\Users\Ryu\Documents\HelloAspNet\HelloBlazorServer\_Imports.razor"
 using HelloBlazorServer.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Ryu\Documents\ASPTest\HelloAspNet\HelloBlazorServer\Pages\FetchData.razor"
+#line 3 "C:\Users\Ryu\Documents\HelloAspNet\HelloBlazorServer\Pages\FetchData.razor"
 using HelloBlazorServer.Data;
 
 #line default
@@ -91,7 +91,7 @@ using HelloBlazorServer.Data;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 39 "C:\Users\Ryu\Documents\ASPTest\HelloAspNet\HelloBlazorServer\Pages\FetchData.razor"
+#line 39 "C:\Users\Ryu\Documents\HelloAspNet\HelloBlazorServer\Pages\FetchData.razor"
        
     private WeatherForecast[] forecasts;
 
