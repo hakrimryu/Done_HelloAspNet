29169e15ab99599892105158ce334bbe80532748
hakrimryu <hakrimryu@gmail.com>
2021-07-25 15:38:40 +0900

    APSEmptyで、MVC基盤作成

---
+++ HelloEmpty/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using HelloEmpty.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloEmpty.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            HelloMessage msg = new HelloMessage()
+            {
+                Message = "Hello MVC Message!"
+            };
+
+            ViewBag.Noti = "Intput message and click submit";
+
+            return View(msg);
+        }
+
+        // POST処理Index
+        [HttpPost]
+        public IActionResult Index(HelloMessage obj)
+        {
+            ViewBag.Noti = "Message Changed";
+            return View(obj);
+        }
+    }
+}
